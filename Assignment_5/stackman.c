da29117952d0e19b0d3333a110f662be1bdc975b
Intrepid194 <spencercollins701@gmail.com>
2026-06-11 22:04:58 -0400

    working on assignment 5

--- Assignment_5/stackman.c
+++ Assignment_5/stackman.c
@@ -35,24 +35,130 @@ int main(int argc, char* argv[]) {
 
     int stack[65536];
 
+    int tos_idx = -1;
+
     char line[1024];
 
     fgets(line, sizeof(line), file);
 
     fclose(file);
 
-    printf("%s", line);
-
     int line_length = 0;
 
     for (int i=0; i<sizeof(line); i++) {
-        line_length++;
-        printf("%c", line[i]);
-        // if (line[i] == "/0") {
-        //     break;
-        // }
+        
+        char command = line[i];
+
+        switch (command) {
+            case '0':
+
+                tos_idx++;
+                stack[tos_idx] = 0;
+                continue;
+
+            case '+':
+
+                if (tos_idx == -1) {
+                    printf("Error: stack underflow");
+                    return 1;
+                }
+                stack[tos_idx]++;
+                continue;
+
+            case '<':
+
+                if (tos_idx == 3) {
+                    printf("Error: stack underflow");
+                    return 1;
+                }
+                continue;
+
+            case '>': {
+                if (tos_idx == -1) {
+                    printf("Error: stack underflow");
+                    return 1;
+                }
+
+                int n = stack[tos_idx];
+
+                if (n < 2 || n > tos_idx) {
+                    printf("Error: stack underflow");
+                    return 1;
+                }
+
+                int mid;
+                int end = tos_idx-1;
+
+                if (n % 2 == 0) {
+                    mid = n /2;
+                } else if (n % 2 > 0) {
+                    mid = (n-1)/2;
+                }
+                
+                for (int i=0; i<mid; i++) {
+                    int temp = stack;
+                }
+                
+                continue;
+            }  
+            case '?': {
+
+                if (tos_idx == -1) {
+                    printf("Error: stack underflow");
+                    return 1;
+                }
+                if (stack[tos_idx] == 0) {
+                    tos_idx++;
+                    stack[tos_idx] = 0;
+                } else if (stack[tos_idx] > 0) {
+                    stack[tos_idx]--;
+                    tos_idx++;
+                    stack[tos_idx] = 1;
+                }
+                continue;
+            }
+            case '_':
+
+                if (tos_idx == -1) {
+                    printf("Error: stack underflow");
+                    return 1;
+                }
+                tos_idx--;
+                continue;
+
+            case '~':
+
+                if (tos_idx == -1) {
+                    printf("Error: stack underflow");
+                    return 1;
+                }
+                int dup = stack[tos_idx];
+                tos_idx++;
+                stack[tos_idx] = dup;
+                continue;
+
+            case '(':
+
+                continue;
+
+            case ')':
+
+            continue;
+
+        }
+        if (line[i] == '\0') {
+            break;
+        }
     }
-    printf("%d", line_length);
+    
+    // if (tos_idx == -1) {
+    //     printf("empty stack");
+    // } else {
+    //     for (int i=0; i<=tos_idx; i++) {
+    //         printf("%d ", stack[i]);
+    //     }
+    // }    
+
     return 0;
 
 }
